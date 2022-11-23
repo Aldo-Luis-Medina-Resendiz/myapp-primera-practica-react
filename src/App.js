4a4c5ed7c50c8f50ad621b5d2440fe62210fd1e4
Aldo-Luis-Medina-Resendiz <lx14981@innovaccion.mx>
2022-11-23 10:51:37 -0600

    Change in the name of the app directory, and change on the readme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from "react-dom/client";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	//Se declaran las variables de diferencia horaria
+	let localtime = new Date();
+	let qatar = new Date(localtime.getTime() + 3600000 * 9);
+	let france = new Date(localtime.getTime() + 3600000 * 7);
+	let japan = new Date(localtime.getTime() + 3600000 * 15);
+
+	//Accedemos al div del html para mostrar los horarios
+	const root = ReactDOM.createRoot(document.getElementById("root"));
+	const element = (
+		<div className="App">
+			<header className="App-header">
+				<h1>La hora es: </h1>
+				<h2>En México : {new Date().toLocaleTimeString("es-MX")}</h2>
+				<h2>En Qatar: {qatar.toLocaleTimeString("es-MX")}</h2>
+				<h2>En Francia: {france.toLocaleTimeString("es-MX")}</h2>
+				<h2>En Japón: {japan.toLocaleTimeString("es-MX")}</h2>
+			</header>
+		</div>
+	);
+	//Renderizamos
+	root.render(element);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React from "react";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+//Llamamos la funcion de app que ya cuenta con todo lo necesario
+//para renderizar y lo ponemos dentro del set interval para mostrar
+//la hora en tiempo real.
+setInterval(App, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
